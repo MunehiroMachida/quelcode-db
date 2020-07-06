40eae73ad4152a808c9736d0b3430d438ecb343a
MUnehiroMachida <Life1231@outlook.jp>
2020-07-06 23:03:19 +0900

    002_SQL分

---
+++ db/db-challenge3/db-002/002.sql
@@ -0,0 +1,30 @@
+
+
+
+
+-- INSERT INTO chatrooms (chatroom_name,chatroom_overview,is_sendable,is_direct_chat,is_deleted,create_user,update_user,created_at,updated_at)
+-- VALUES ()    テンプレ
+
+INSERT INTO chatrooms (chatroom_name,chatroom_overview,create_user,update_user,created_at,updated_at)
+VALUES ('りんご','利用者の集まり',1,1,'2020/04/21 11:26:32','2020/04/21 11:26:32');
+
+INSERT INTO chatrooms (chatroom_name,chatroom_overview,create_user,update_user,created_at,updated_at)
+VALUES ('ごりら','sqlの集まり',2,2,'2020/04/23 15:42:05','2020/04/23 15:42:05');
+
+INSERT INTO chatrooms (chatroom_name,chatroom_overview,create_user,update_user,created_at,updated_at)
+VALUES ('らっぱ','言語の集まり',8,9,'2020/04/24 11:03:16','2020/04/25 10:33:55');
+
+INSERT INTO chatrooms (chatroom_name,chatroom_overview,is_sendable,create_user,update_user,created_at,updated_at)
+VALUES ('ぱんだ','フレームワークの集まり',0,5,7,'2020/04/23 18:14:28','2020/04/24 20:33:06');
+
+INSERT INTO chatrooms (chatroom_name,chatroom_overview,is_direct_chat,create_user,update_user,created_at,updated_at)
+VALUES ('ごま','adminとmysqlのダイレクトチャット',1,1,1,'2020/04/23 14:34:58','2020/04/23 14:34:58');
+
+INSERT INTO chatrooms (chatroom_name,chatroom_overview,is_sendable,is_direct_chat,create_user,update_user,created_at,updated_at)
+VALUES ('まり','adminとsqlserverのダイレクトチャット',0,1,1,1,'2020/04/23 14:45:22','2020/04/23 14:45:22');
+
+INSERT INTO chatrooms (chatroom_name,chatroom_overview,is_direct_chat,create_user,update_user,created_at,updated_at)
+VALUES ('りす','adminとjsのダイレクトチャット',1,1,1,'2020/04/24 10:55:21','2020/04/24 10:55:21');
+
+INSERT INTO chatrooms (chatroom_name,chatroom_overview,is_direct_chat,is_deleted,create_user,update_user,created_at,updated_at)
+VALUES ('かめ','adminとamazonのダイレクトチャット',1,1,1,1,'2020/04/24 11:02:20','2020/04/27 15:41:33');
