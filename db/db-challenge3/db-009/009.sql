2ac6eda858aa75156bc2655f0f57f77c5d0f15ff
MUnehiroMachida <Life1231@outlook.jp>
2020-07-07 17:39:36 +0900

    009.sql&画像追加

---
+++ db/db-challenge3/db-009/009.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(posts.chatroom_id) AS 投稿数,chatrooms.chatroom_name
+FROM posts
+JOIN chatrooms
+ON posts.chatroom_id = chatrooms.id
+JOIN users_informations
+ON posts.create_user = users_informations.id
+WHERE posts.is_deleted NOT IN (1)
+AND users_informations.is_deleted NOT IN (1)
+GROUP BY posts.chatroom_id;
