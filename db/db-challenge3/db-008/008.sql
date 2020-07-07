cc9e0b05841f880dbe55e336bdb3f34118470d9d
MUnehiroMachida <Life1231@outlook.jp>
2020-07-07 17:23:54 +0900

    008._sql&画像追加

---
+++ db/db-challenge3/db-008/008.sql
@@ -0,0 +1,6 @@
+SELECT users_informations.display_name,chatrooms.chatroom_name,CAST(participants.participation_date AS DATE)
+FROM `participants`
+JOIN `users_informations`
+ON participants.user_id = users_informations.id
+JOIN `chatrooms`
+ON participants.chatroom_id = chatrooms.id;
