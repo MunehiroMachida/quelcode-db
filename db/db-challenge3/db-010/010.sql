fb42d826c124a0017b13a6a9f7b8bf37c8c8ae2b
MUnehiroMachida <Life1231@outlook.jp>
2020-07-09 14:51:30 +0900

    微修正&ORDER_BY追加&画像修正

--- db/db-challenge3/db-010/010.sql
+++ db/db-challenge3/db-010/010.sql
@@ -1,8 +1,9 @@
 SELECT U.display_name AS ユーザー名,C.chatroom_name AS チャットルーム名,P_2.created_at AS 投稿日時
 FROM posts AS P_1
-JOIN (SELECT chatroom_id,MAX(created_at) AS created_at FROM posts WHERE iis_deleted = 0 GROUP BY chatroom_id)AS P_2
+JOIN (SELECT chatroom_id,MAX(created_at) AS created_at FROM posts WHERE is_deleted = 0 GROUP BY chatroom_id)AS P_2
 ON P_1.created_at = P_2.created_at
 JOIN chatrooms AS C
 ON P_2.chatroom_id = C.id
 JOIN users_informations AS U
 ON P_1.create_user = U.id
+ORDER BY C.id ASC;
