629d86d0081dbf94f6174d9a68b118d21abe5345
MUnehiroMachida <Life1231@outlook.jp>
2020-07-09 14:29:46 +0900

    007_ORDER_BY追加&スクショ修正

--- db/db-challenge3/db-007/007.sql
+++ db/db-challenge3/db-007/007.sql
@@ -3,4 +3,5 @@ CASE is_sendable WHEN '0' THEN '禁止'
 WHEN '1' THEN '許可' END AS ファイル送信,is_direct_chat,is_deleted,create_user,update_user,created_at,updated_at
 FROM `chatrooms`
 WHERE is_deleted = 0
-AND is_direct_chat = 1;
+AND is_direct_chat = 1
+ORDER BY id ASC;
